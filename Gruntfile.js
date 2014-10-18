89ab10ae495d928007f3995d6a6f2525f1a49d1a
Jabran Rafique <hello@jabran.me>
2014-10-18 09:07:52 +0100

    $1
    add files

---
+++ Gruntfile.js
@@ -0,0 +1,91 @@
+// Boilerplate that enables PHP built-in server as Grunt task along with optional Sass and JS tasks.
+
+module.exports = function(grunt) {
+
+	// Load all tasks
+	require('load-grunt-tasks')(grunt);
+
+	// Setup app path
+	var config = {
+		app: 'app'
+	};
+
+	grunt.initConfig({
+
+		// Read package file for later reuse
+		pkg: grunt.file.readJSON('package.json'),
+
+		// Copy params to local scope
+		config: config,
+
+		sass: {
+			dist: {
+				files: {
+					'<%= config.app %>/css/style.css':'<%= config.app %>/sass/{,*/}*.{scss,sass}'
+				},
+				options: {
+					style: 'expanded'
+				}
+			}
+		},
+
+		cssmin: {
+			dist: {
+				files: {
+					'<%= config.app %>/css/style.min.css':['<%= config.app %>/css/style.css']
+				},
+				options: {
+					banner: '/*! <%= pkg.name %> | v<%= pkg.version %> | <%= pkg.author %> | <%= pkg.homepage %> | <%= pkg.license %> */ \n'
+				}
+			}
+		},
+
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> | v<%= pkg.version %> | <%= pkg.author %> | <%= pkg.homepage %> | <%= pkg.license %> */ \n',
+				preserveComments: 'some'
+			},
+			build: {
+				src: '<%= config.app %>/js/main.js',
+				dest: '<%= config.app %>/js/main.min.js',
+			}
+		},
+
+		php: {
+			options: {
+				base: '<%= config.app %>',
+				hostname: 'localhost',
+				livereload: 35729,
+				open: true
+			},
+			watch: {}
+		},
+
+		watch: {
+			sass: {
+				files: ['<%= config.app %>/sass/{,*/}*.{scss,sass}'],
+				tasks: ['sass'],
+				options: {
+					spawn: false
+				}
+			}
+		}
+	});
+
+	// Register all tasks
+	grunt.registerTask('default', [
+		'php:watch',
+		'watch'
+	]);
+
+	grunt.registerTask('serve', [
+		'php:watch',
+		'watch'
+	]);
+
+	grunt.registerTask('build', [
+		'sass:dist',
+		'cssmin:dist',
+		'uglify'
+	]);
+};
--- README.md
+++ README.md
@@ -1,4 +1,39 @@
-php-server-boilerplate
+PHP Server Boilerplate
 ======================
 
 A boilerplate that enables PHP built-in server as Grunt task along with optional Sass and JS tasks.
+
+# Installation & How to Use
+
+[Download the latest stable copy](https://github.com/jabranr/php-server-boilerplate/releases)
+
+#### For development, use: `$ grunt` or `$ grunt serve`
+
+This will run PHP built-in server (requires PHP 5.4+ installation) and watch for requests. Additional task will watch for changes in Sass files (.scss, .sass) and convert them to CSS.
+
+#### For production, use: `$ grunt build`
+
+This will compress and minify the CSS and JS files for production use. It will not watch for changes or run a server.
+
+**Important:** Always make changes to Sass files. `$ grunt build` will override CSS file as it will convert the current state of Sass to CSS before compressing and minifying them.
+
+This boilerplate has following development dependencies.
+
++ [grunt](https://www.npmjs.org/package/grunt)
++ [grunt-contrib-cssmin](https://www.npmjs.org/package/grunt-contrib-cssmin)
++ [grunt-contrib-sass](https://www.npmjs.org/package/grunt-contrib-sass)
++ [grunt-contrib-uglify](https://www.npmjs.org/package/grunt-contrib-uglify)
++ [grunt-contrib-watch](https://www.npmjs.org/package/grunt-contrib-watch)
++ [grunt-php](https://www.npmjs.org/package/grunt-php)
++ [load-grunt-tasks](https://www.npmjs.org/package/load-grunt-tasks)
+
+# Contribute
+
+Feel free to fork, contribute, redistribute etc.
+
+Use standard procedure:
+Fork -> checkout branch -> add feature -> commit & create pull request -> :+1:
+
+# License
+
+MIT License
